85d7763ff5daaf1c9dc4d6c226199130ccdcdbb7
Asif Saif Uddin <auvipy@gmail.com>
2021-10-07 12:11:38 +0600

    initial basic working users API endpoint without generic filtering

--- jobport/candidates/views.py
+++ jobport/candidates/views.py
@@ -1,7 +1,7 @@
-from django.contrib.auth.models import User,
+from django.contrib.auth.models import User
 from rest_framework import viewsets
 from rest_framework import permissions
-from . serializers import UserSerializer,
+from . serializer import UserSerializer
 
 
 class UserViewSet(viewsets.ModelViewSet):
