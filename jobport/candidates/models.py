19f7f2f171b397d681988714cde9a40bf8d0540b
Asif Saif Uddin <auvipy@gmail.com>
2021-10-07 10:06:14 +0600

    basic models systems Users

--- jobport/candidates/models.py
+++ jobport/candidates/models.py
@@ -1,3 +1,29 @@
+# Create your models here.
+
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+from accounts.managers import UserManager
+
+GENDER_CHOICES = (("male", "Male"), ("female", "Female"))
+
+
+class User(AbstractUser):
+    username = None
+    role = models.CharField(max_length=12, error_messages={"required": "Role must be provided"})
+    gender = models.CharField(max_length=10, blank=True, null=True, default="")
+    email = models.EmailField(
+        unique=True,
+        blank=False,
+        error_messages={
+            "unique": "A user with that email already exists.",
+        },
+    )
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+
+    def __str__(self):
+        return self.email
+
+    objects = UserManager()
