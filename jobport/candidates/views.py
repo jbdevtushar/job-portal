c23788ddc91b190d5426a1ab85f90c539f4c2147
Asif Saif Uddin <auvipy@gmail.com>
2021-10-07 12:44:37 +0600

    basic filtering with adminrenderer

--- jobport/candidates/views.py
+++ jobport/candidates/views.py
@@ -1,7 +1,9 @@
 from django.contrib.auth.models import User
 from rest_framework import viewsets
 from rest_framework import permissions
+from rest_framework.renderers import AdminRenderer
 from . serializer import UserSerializer
+from django_filters import rest_framework as filters
 
 
 class UserViewSet(viewsets.ModelViewSet):
@@ -11,3 +13,5 @@ class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all().order_by('-date_joined')
     serializer_class = UserSerializer
     permission_classes = [permissions.IsAuthenticated]
+    renderer_classes = [AdminRenderer]
+    filterset_fields = ('username', 'email',)
--- jobport/jobport/settings.py
+++ jobport/jobport/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'django_filters',
     # projects specific apps
     'candidates.apps.CandidatesConfig',
 ]
@@ -130,5 +131,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 10
+    'PAGE_SIZE': 10,
+    'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend']
 }
