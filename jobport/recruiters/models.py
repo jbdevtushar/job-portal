431b743ed457e5bd8c8de06c39ee9bac55664e40
Asif Saif Uddin <auvipy@gmail.com>
2021-10-11 14:14:03 +0600

    more base models for recruiter for asignment without field types and appropriate model table relations

--- jobport/recruiters/models.py
+++ jobport/recruiters/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser, User
 
-# Create your models here.
+# the following model can be split in many others moderls & 
+# have appropriate relations as per need
+
+class Recruiters(models.Model):
+	user = None
+	job_title = None
+	company_info = None
+	address = None
+	contact_info = None
+	website = None
